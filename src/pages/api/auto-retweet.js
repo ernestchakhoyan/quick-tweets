fb85b7984c3c0c2999f52f2173763877628d8bc0
ernest <echakhoyan@bluip.com>
2020-10-11 15:53:41 +0400

    Hot fix

--- src/pages/api/auto-retweet.js
+++ src/pages/api/auto-retweet.js
@@ -2,10 +2,7 @@ import { RunTwitter } from "../../services/twitter/retweet";
 
 export default (req, res) => {
     try{
-        // const  {access_token, access_token_secret} = req.body;
-
-        const access_token = "1000695336497811457-ARYAeMACCEhvoL7plUWqCl5j6VdSmH";
-        const access_token_secret =  "YWhILwsCs2rSOHvUwpCzt3Z32nFbYW2DYvR4cIWxB7ZTM";
+        const  {access_token, access_token_secret} = req.body;
 
         RunTwitter({access_token, access_token_secret});
         res.status(200).json({ success: true })
