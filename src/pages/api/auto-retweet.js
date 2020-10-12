e5c4daafdcb251c58d7db988bf1737487fa2b894
ernest <echakhoyan@bluip.com>
2020-10-13 02:15:39 +0400

    Access token

--- src/pages/api/auto-retweet.js
+++ src/pages/api/auto-retweet.js
@@ -2,7 +2,9 @@ import { RunTwitter } from "../../socials/twitter/retweet";
 
 export default async (req, res) => {
     try {
-        const { access_token, access_token_secret } = req.body;
+        // const { access_token, access_token_secret } = req.body;
+        const access_token = "1000695336497811457-ARYAeMACCEhvoL7plUWqCl5j6VdSmH";
+        const access_token_secret =  "YWhILwsCs2rSOHvUwpCzt3Z32nFbYW2DYvR4cIWxB7ZTM";
         const a = await RunTwitter({ access_token, access_token_secret });
         console.log(a,"aaa");
         res.status(200).json({ success: true, stream: a });
