d719f68b4e5997a60c17c49f93654bc6a83caa03
ernest <echakhoyan@bluip.com>
2020-10-13 04:20:21 +0400

    a

--- src/socials/twitter/index.js
+++ src/socials/twitter/index.js
@@ -9,5 +9,6 @@ export const initTwit = ({access_token, access_token_secret}) => {
         access_token_secret,
         timeout_ms: 60 * 1000,
         strictSSL: true,
+        _twitter_time_minus_local_time_ms: 4
     })
 }
