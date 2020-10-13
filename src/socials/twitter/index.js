51a17c47dff39d8518e2b01c57d7a6e426e748ad
ernest <echakhoyan@bluip.com>
2020-10-13 12:53:15 +0400

    Vercel commit

--- src/socials/twitter/index.js
+++ src/socials/twitter/index.js
@@ -8,7 +8,6 @@ export const initTwit = ({access_token, access_token_secret}) => {
         access_token,
         access_token_secret,
         timeout_ms: 60 * 1000,
-        strictSSL: true,
-        _twitter_time_minus_local_time_ms: 4
+        strictSSL: true
     })
 }
